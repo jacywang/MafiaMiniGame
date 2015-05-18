f779e2ae8b2cc344435ba02b69c49c379e13bcdb
Jacy Wang <jacywj@gmail.com>
2015-05-18 15:11:57 -0700

    Initialize repo

---
+++ MafiaMiniGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MafiaMiniGame
+//
+//  Created by JIAN WANG on 5/18/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
